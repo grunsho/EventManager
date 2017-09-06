fb23517815de3d8b6febf4c379f9d928d3d5420d
Alvaro Manterola <alvaro.manterola@gmail.com>
2017-09-06 17:09:12 -0300

    Saliendo de la oficina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +18,25 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
-  }
-}
 
+    firebase.initializeApp({
+      apiKey: "AIzaSyB1uxfqMwMeG0UOuG4KKVY1IFAfJNFXYr0",
+      authDomain: "eventmanager-21a5e.firebaseapp.com",
+      databaseURL: "https://eventmanager-21a5e.firebaseio.com",
+      projectId: "eventmanager-21a5e",
+      storageBucket: "",
+      messagingSenderId: "716727941635"
+    });
+
+
+  const unsuscribe = firebase.auth().onAuthStateChanged( user => {
+    if(!user){
+      this.rootPage = 'LoginPage';
+      unsuscribe();
+    } else {
+      this.rootPage = HomePage;
+      unsuscribe();
+    }
+  });
+}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+import { EventProvider } from '../providers/event/event';
+import { ProfileProvider } from '../providers/profile/profile';
+
+class CameraMock extends Camera {
+  getPicture(options) {
+    return new Promise( (resolve, reject) => {
+      resolve('TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=');
+    });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +36,12 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: Camera, useClass: CameraMock},
+    AuthProvider,
+    EventProvider,
+    ProfileProvider
   ]
 })
 export class AppModule {}
